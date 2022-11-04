4acf797103907d99caf15b97cb6edeff45c0de7b
Esteban <esteban.escalante@utp.edu.co>
2022-11-03 20:06:20 -0500

    r

---
+++ file.php
@@ -0,0 +1,23 @@
+<?php
+if (($a ==1) || (3!=5)){
+$c = 5;
+}
+
+echo "hello world";
+
+
+/*
+SOY un COMENTARIO ALV
+Coloque todo lo que usted prefiera
+en este comentario
+*/
+
+function foo($arg1,$a,$arg2){
+	if (($arg1 == 1) || ($a!=$arg2)){
+		$a = 5;
+	}else{
+	    $a = $arg1;
+	}
+}
+
+?>
---
+++ prueba.php
@@ -0,0 +1,15 @@
+<?php
+ 
+ echo "!Hola mundo! ";
+ 
+ for($i = 0; $i < 10; $i++)
+  {
+   echo "Línea número: ";
+  }
+
+// Esteban
+# Esteban
+/* esteban */
+ 
+
+?>
